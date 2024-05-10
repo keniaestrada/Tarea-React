862bf4d9ff6db30ea2d111a7b510372a799880d5
keniaestrada <kenialbsg@gmail.com>
2024-05-09 18:58:10 -0600

    tarjeta

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,13 +1,29 @@
+import React from 'react';
 import './Main.css';
 
+const Main = () => {
+    const datosPersonales = {
+        nombre: "Juan Pérez",
+        edad: 30,
+        ocupacion: "Desarrollador Web",
+        correo: "juan@example.com",
+        fotoUrl: "https://via.placeholder.com/150",
+    };
 
-const Main = () => (
-    <>
-        <h1>Hola mundo</h1>
-        <h2>Aprendiendo React</h2>
-        <p><small>Help</small></p>
-    </>
-)
-
+    return (
+        <>
+            <h1>My best pet</h1>
+            <h2>Aprendiendo React</h2>
+            <p><small>Help</small></p>
+            <div className="tarjeta">
+                <img src={datosPersonales.fotoUrl} alt="Foto de perfil" />
+                <h3>{datosPersonales.nombre}</h3>
+                <p>Edad: {datosPersonales.edad}</p>
+                <p>Ocupación: {datosPersonales.ocupacion}</p>
+                <p>Correo: {datosPersonales.correo}</p>
+            </div>
+        </>
+    );
+};
 
 export default Main;
